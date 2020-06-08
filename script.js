6fa6dad1f6ee5315e41168506b3135f50ff0ccce
Alexander.German <questlogilt@mail.ru>
2020-06-08 11:47:23 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let	money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let	money = 500 , 
+    income = " без дополнительного дохода ", 
+    addExpenses = "Кварплата", 
+    deposit = 1000, 
+    mission = 5000, 
+    period = 12,
+    budgetDay = money / 30;
+
+    console.log("income:", income);
+    console.log("money:", money);
+    console.log("deposit:", deposit);
+    console.log("addExpenses length:", addExpenses.length);
+    console.log("Период равен: " + period + " месяцев");
+    console.log("Цель заработать: " + mission + " гривен");
+    console.log("budgetDay:", budgetDay)
 
 
-alert(' Привет Макс '); 
+
