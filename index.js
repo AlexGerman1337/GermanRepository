632c9011a6a431f8f45b8d97c021c865a880affe
Alexander.German <questlogilt@mail.ru>
2020-06-18 13:53:29 +0300

    lesson09

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 
+let salaryAmount = document.querySelector('.salary-amount');
+let start = document.getElementById('start');
+let button01 = document.getElementsByTagName('button')[0];
+let button02 = document.getElementsByTagName('button')[1];
+let depositCheckmark = document.querySelector('.deposit-checkmark');
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+let incomeItems = document.querySelectorAll('.income-items');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let targetAmount = document.querySelector('.target-amount');
+let periodSelect = document.querySelector('.period-select');
+let budgetMonthValue = document.getElementsByClassName("budget_month-value");
+let budgetDayValue = document.getElementsByClassName("budget_day-value");
+let expensesMonthValue = document.getElementsByClassName("expenses_month-value");
+let additionalIncomeValue = document.getElementsByClassName("additional_income-value");
+let additionalExpensesValue = document.getElementsByClassName("additional_expenses-value");
+let incomePeriodValue = document.getElementsByClassName("income_period-value");
+let targetMonthValue = document.getElementsByClassName("target_month-value");
 
-
-console.log(document.querySelector('.salary-amount'));
-console.log(document.getElementById('start'));
-console.log(document.getElementsByTagName('button')[0]);
-console.log(document.getElementsByTagName('button')[1]);
-console.log(document.querySelector('.deposit-checkmark'));
-console.log(document.querySelectorAll('.additional_income-item'));
-console.log(document.querySelectorAll('.income-items'));
-console.log(document.querySelectorAll('.expenses-items'));
-console.log(document.querySelector('.additional_expenses-item'));
-console.log(document.querySelector('.target-amount'));
-console.log(document.querySelector('.period-select'));
-
-
-
-console.log(document.getElementsByClassName("budget_month-value"));
-console.log(document.getElementsByClassName("budget_day-value"));
-console.log(document.getElementsByClassName("expenses_month-value"));
-console.log(document.getElementsByClassName("additional_income-value"));
-console.log(document.getElementsByClassName("additional_expenses-value"));
-console.log(document.getElementsByClassName("income_period-value"));
-console.log(document.getElementsByClassName("target_month-value"));
+console.log(salaryAmount);
+console.log(start);
+console.log(button01);
+console.log(button02);
+console.log(depositCheckmark);
+console.log(additionalIncomeItem);
+console.log(incomeItems);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodSelect);
+console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
 
 
 
